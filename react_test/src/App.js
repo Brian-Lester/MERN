9c077a62595f9383f9641f0622903148adbb702e
Brian-Lester <blester426@gmail.com>
2023-04-22 11:13:25 -0400

    learning react

---
+++ react_test/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things i need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Remake solo project</li>
+        <li>
+          remake group project
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
