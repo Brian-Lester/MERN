b608757b7ac284228d1716d5b63eff52f7c80b5a
Brian-Lester <blester426@gmail.com>
2023-05-14 21:56:23 -0400

    added validations

--- todo-list/src/Components/Form.jsx
+++ todo-list/src/Components/Form.jsx
@@ -16,18 +16,30 @@ const Form = (props) => {
         dueDate:""})
     }
 
+
+
     return (
         <div>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossOrigin="anonymous"/>
         <form onSubmit={submitHandler}>
                 <div className="mb-3">
                 <label className= "form-label">Add an Item:</label>
+                {
+                    item.Title.length < 3?
+                    <p>Please provide a Todo</p>:
+                    null
+                }
                 <input type = "text" onChange={changeHandler}  name = "Title" value = {item.Title } className='form-control'></input>
                 </div>
                 <div className="mb-3">
                 <label className ="form-label">
                     When does this need to be done?
                 </label>
+                {
+                    new Date(item.dueDate) < new Date()?
+                    <p>date Cant be in the past</p>:
+                    null
+                }
                 <input type ="date" onChange={changeHandler} name = "dueDate" value ={item.dueDate} className='form-control'></input>
                 </div>
                 <button type="submit" className="btn btn-outline-primary">Submit</button>
